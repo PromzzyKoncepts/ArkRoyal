baac319c515c86c8a9d399f21d731cb68659cd35
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-09-07 23:25:30 +0100

    component for people about section

---
+++ src/src/data/People.jsx
@@ -0,0 +1,90 @@
+import nonso from "../../assets/nonso.jpg";
+import ramzy from "../../assets/ramzy.jpg";
+import pascal from "../../assets/pascal.jpg";
+import charles from "../../assets/charles.jpg";
+import Hillary from "../../assets/Hillary.jpg";
+import ifeanyi from "../../assets/ifeanyi.jpg";
+import jkings from "../../assets/Jkings.jpg";
+
+
+const People = [
+  {
+    name: "Hon Eneh Paschal Nduka",
+    profession: "Fashion designer",
+    company: "CEO, Puzzle Classic Clothing",
+    position: "Chairman, ArkRoyal Club",
+    picture: pascal,
+    fbUrl: 'https://facebook.com',
+    IGUrl: 'https://instagram.com',
+    linkedIn: 'https://linkedin.com'
+
+  },
+  {
+    name: "Nnenanya Hillary Chukwuebuka ",
+    profession: "Entrepreneur",
+    company: "G-max Premium Nig. Ltd & G-max premium table water",
+    company2: "MD - Raco Moon Intl. Ltd",
+    position: "Secretary - Ark Royal Club",
+    picture: Hillary,
+    fbUrl: 'https://facebook.com',
+    IGUrl: 'https://instagram.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    name: "Charles C. Egumgbe, Esq",
+    profession: "Legal Practitioner/Consultant",
+    company: "Managing Partner, Egumgbe, Egumgbe & Associates",
+    company2: "MD - Raco Moon Intl. Ltd",
+    position: "Financial Secretary - Ark Royal Club",
+    picture: charles,
+    fbUrl: 'https://facebook.com',
+    IGUrl: 'https://instagram.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    name: "Duby-Odeh Ramsey",
+    profession: "Businessman/Contractor",
+    company: "Managing Director Gazstrom Energy Ltd",
+    company2: "",
+    position: "P.R.O Ark Royal Club",
+    picture: ramzy,
+    fbUrl: 'https://facebook.com',
+    IGUrl: 'https://instagram.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    name: "AMB OZONWACHUKWU U.J",
+    profession: "Realtor/Contractor ",
+    company: "C.E.O Jkings Properties Estate Consultant",
+    company2: "",
+    position: "Top-Member Ark Royal Club",
+    picture: jkings,
+    fbUrl: 'https://facebook.com',
+    IGUrl: 'https://instagram.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    name: "Ifeacho Chukwunonso Gerald",
+    profession: "Businessman",
+    company: "CEO Nolson Konsept international",
+    company2: "",
+    position: "Top-Member Ark Royal Club",
+    picture: nonso,
+    fbUrl: 'https://facebook.com',
+    IGUrl: 'https://instagram.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    name: "Okigwe Ifeanyi",
+    profession: "Foreign Exchange Broker",
+    company: "C.E.O ",
+    company2: "",
+    position: "Top-Member Ark Royal Club",
+    picture: ifeanyi,
+    fbUrl: 'https://facebook.com',
+    IGUrl: 'https://instagram.com',
+    linkedIn: 'https://linkedin.com'
+  },
+];
+
+export default People;
