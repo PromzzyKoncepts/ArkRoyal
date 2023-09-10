704b7e12478372311b8b067ac5ab107957a529cd
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-09-10 22:45:13 +0100

    add page for components

---
+++ src/src/Pages/Projects.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+import "../../Stylesheets/About.css";
+import Header from "../components/Header";
+
+const Projects = () => {
+  return (
+    <div className="blogContainer">
+      <Header />
+      <div>
+        <h1>PROJECTS COMING SOON!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
