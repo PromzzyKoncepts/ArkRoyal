13b72a6a92b9a3fa682ef3269f2d4258be71a034
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-09-10 22:44:54 +0100

    add page for components

---
+++ src/src/Pages/Blog.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+import '../../Stylesheets/About.css'
+import Header from '../components/Header'
+
+const Blog = () => {
+  return (
+    <div className="blogContainer">
+      <Header />
+      <div>
+      <h1>COMING SOON!</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default Blog
