f3ee40a1fa63857ecd34da463246a5070a4b81e3
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-09-10 22:42:58 +0100

    add data source file

---
+++ src/src/data/FeaturesData.jsx
@@ -0,0 +1,47 @@
+import AddBusinessIcon from "@mui/icons-material/AddBusiness";
+import SportsVolleyballIcon from "@mui/icons-material/SportsVolleyball";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import HandshakeIcon from "@mui/icons-material/Handshake";
+import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
+import PeopleIcon from "@mui/icons-material/People";
+
+const Features = [
+  {
+    title: "Community Building",
+    icon: <AddBusinessIcon fontSize="large" />,
+    details:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nobis cumque reiciendis unde ut debitis, voluptatem quis eaque aspernatur dolorem. Fugit impedit asperiores repellendus soluta quos eos nobis, labore volup in doloribus",
+  },
+  {
+    title: "Recreation & Entertainment",
+    icon: <SportsVolleyballIcon fontSize="large" />,
+    details:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nobis cumque reiciendis unde ut debitis, voluptatem quis eaque aspernatur dolorem. Fugit impedit asperiores repellendus soluta quos eos nobis, labore volup in doloribus",
+  },
+  {
+    title: "Health & Wellness",
+    icon: <HealthAndSafetyIcon fontSize="large" />,
+    details:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nobis cumque reiciendis unde ut debitis, voluptatem",
+  },
+  {
+    title: "Volunteerism & Giving back",
+    icon: <HandshakeIcon fontSize="large" />,
+    details:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nobis ",
+  },
+  {
+    title: "Innovation and Discussion",
+    icon: <EmojiObjectsIcon fontSize="large" />,
+    details:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nobis cumque reiciendis unde ut debitis, voluptatem quis eaque aspernatur dolorem. Fugit impedit asperiores repellendus soluta quos eos nobis, labore volup in doloribus",
+  },
+  {
+    title: "Cultural Exchange",
+    icon: <PeopleIcon fontSize="large" />,
+    details:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nobis cumque reiciendis unde ut debitis, voluptatem quis eaque aspernatur dolorem. Fugit impedit asperiores repellendus soluta quos eos nobis, labore volup in doloribus",
+  },
+];
+
+export default Features;
